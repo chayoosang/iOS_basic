185c248ecc664ae88dbd74ee3ce8a740b355e6af
yoosang <yoo7969@naver.com>
2023-01-01 15:24:21 +0900

    Closure 강의 2

--- ios_1.playground/Pages/Closure.xcplaygroundpage/Contents.swift
+++ ios_1.playground/Pages/Closure.xcplaygroundpage/Contents.swift
@@ -37,8 +37,6 @@ func f2(){
 
 f1()
 
-
-
 func f10(myClosure : () -> Void) {
     print("작업중...")
     print("작업중...")
@@ -55,3 +53,95 @@ f10(myClosure: { f20() } )
 f10 {f20()}
 
 f10 {print("내가 원하는 로직")}
+
+
+
+// Closure2
+
+// func + closure2
+func myFunc2(closure1: () -> Void, closure2: () -> Void){
+    print("func2")
+    
+    closure1()
+    closure2()
+}
+
+myFunc2 {
+    print("closure1")
+} closure2: {
+    print("closure2")
+}
+
+
+// func + closure3
+func myFunc3(closure1: () -> Void, closure2: () -> Void, closure3: () -> Void){
+    print("func3")
+    
+    closure1()
+    closure2()
+    closure3()
+}
+
+myFunc3 {
+    print("c1")
+} closure2: {
+    print("c2")
+} closure3: {
+    print("c3")
+}
+
+func myFunc4(i: Int, closure: () -> Void){
+    print(i)
+    closure()
+}
+
+myFunc4(i: 50) {
+    print("closure")
+}
+
+
+// closure + parmeter
+
+func myFunc5(completion: (Int) -> Void) {
+    print("func...")
+    let total = 100
+    completion(total)
+}
+
+myFunc5 { total in
+    print(total.description + "점")
+}
+
+func showScore(a: Int) {
+    print((a + 10).description + "점")
+}
+
+myFunc5(completion: showScore)
+
+
+// closure + return
+
+func myFunc6(completion : () -> Int) {
+    print("myFunc6")
+    let a = completion()
+    print(a.description)
+}
+
+myFunc6 {
+    return 10
+}
+
+
+func myFunc7(completion : (Int) -> Int) {
+    print("myFunc7")
+    let a = completion(10)
+    print(a.description)
+}
+
+myFunc7 { num in
+    return num + 10
+}
+
+// Closure 3
+
+// Capturing Values
